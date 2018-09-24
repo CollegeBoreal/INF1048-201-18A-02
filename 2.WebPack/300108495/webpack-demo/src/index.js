195bf424b807fbc005efd5ed727c8c63873ec0be
safaa <31968900+Safaazaoui@users.noreply.github.com>
2018-09-24 12:57:28 -0400

    Update index.js

--- 2.WebPack/300108495/webpack-demo/src/index.js
+++ 2.WebPack/300108495/webpack-demo/src/index.js
@@ -1,5 +1,5 @@
 function component() {
-    let element = documet.createElement('div');
+    let element = document.createElement('div');
      element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     return element;
 }
