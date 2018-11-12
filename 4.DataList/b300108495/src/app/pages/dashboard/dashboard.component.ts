03e5af8f778578291f53ba8e226bd65ce80ffc94
safaa <safaazaoui@gmail.com>
2018-11-12 13:59:14 -0500

    safaa

--- 4.DataList/b300108495/src/app/pages/dashboard/dashboard.component.ts
+++ 4.DataList/b300108495/src/app/pages/dashboard/dashboard.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  name: String;
 
   constructor() { }
 
   ngOnInit() {
+    this.name = 'safaa';
   }
 
 }
