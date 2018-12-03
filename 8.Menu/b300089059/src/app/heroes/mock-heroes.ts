ebe70cc49b50cd7d5d30dc23567f7c6fdccb9171
fabienpaul <fabien.hoachuck@gmail.com>
2018-12-03 12:26:09 -0500

    menu

---
+++ 8.Menu/b300089059/src/app/heroes/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Brice' },
+  { id: 2, name: 'Safaa' },
+  { id: 3, name: 'Amelie' },
+  { id: 4, name: 'Hamido' },
+  { id: 5, name: 'Fabien' },
+  { id: 6, name: 'Mostafa' }
+
+];
