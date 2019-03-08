af85ce050ec210fbd04ffd0ef16f388290b65f7d
Setra <setrar@valiha.com>
2019-03-08 13:28:06 -0500

    .

--- 4.DataList/b300098957/src/app/app-routing.module.ts
+++ 4.DataList/b300098957/src/app/app-routing.module.ts
@@ -1,10 +1,45 @@
 import { NgModule } from '@angular/core';
 import {Routes, RouterModule, ExtraOptions} from '@angular/router';
+import {
+  NbAuthComponent, NbLoginComponent, NbLogoutComponent, NbRegisterComponent,
+  NbRequestPasswordComponent, NbResetPasswordComponent
+} from '@nebular/auth';
 
 const routes: Routes = [
   { path: 'pages', loadChildren: './pages/pages.module#PagesModule' },
   { path: '', redirectTo: 'pages', pathMatch: 'full' },
   { path: '**', redirectTo: 'pages' },
+  {
+    path: 'auth',
+    component: NbAuthComponent,
+    children: [
+      {
+        path: '',
+        component: NbLoginComponent,
+      },
+      {
+        path: 'login',
+        component: NbLoginComponent,
+      },
+      {
+        path: 'register',
+        component: NbRegisterComponent,
+      },
+      {
+        path: 'logout',
+        component: NbLogoutComponent,
+      },
+      {
+        path: 'request-password',
+        component: NbRequestPasswordComponent,
+      },
+      {
+        path: 'reset-password',
+        component: NbResetPasswordComponent,
+      },
+    ],
+  },
+
 ];
 
 const config: ExtraOptions = {
